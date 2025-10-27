0b9a8ffd27d10b64685a0859ee234ea0374520ab
greg-suehr <gregory.suehr@gmail.com>
2025-10-27 12:59:35 -0400

    manual client page p1

---
+++ src/Controller/Samson/SamsonController.php
@@ -0,0 +1,43 @@
+<?php
+  
+namespace App\Controller\Samson;
+
+use App\Service\SiteContext;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class SamsonController extends AbstractController
+{  
+  public function __construct(    
+    private SiteContext $siteContext,
+  )
+  {}
+
+  #[Route('/home', name: 'samson_home')]
+  public function home(Request $request): Response
+  {
+    return $this->render('samson/home.html.twig');
+  }
+
+  #[Route('/bio', name: 'samson_bio')]
+  public function bio(Request $request): Response
+  {
+    return $this->render('samson/bio.html.twig');
+  }
+
+  #[Route('/media', name: 'samson_media')]
+  public function media(Request $request): Response
+  {
+    return $this->render('samson/media.html.twig');
+  }
+
+  // TODO: store and map individual media pages
+  #[Route('/media/{id}', name: 'samson_media_view')]
+  public function media(Request $request): Response
+  {
+    return $this->render('samson/media.html.twig');
+  }
+}
